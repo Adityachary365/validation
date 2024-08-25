a1ee0b1b8677d8286afea643f4f7b4f6ebc7981e
Adityachary365 <charybros12345@gmail.com>
2024-08-25 06:35:33 -0700

    Update validation.js

--- validation.js
+++ validation.js
@@ -7,21 +7,21 @@ function check(){
     let birthdate=document.getElementById("bday").value;
     phone=Number(phone);
     if(username==""){
-        window.alert("Username cant be empty!");
+        alert("Username cant be empty!");
         x-=1;
     }
     if(password==""){
-        window.alert("Password cant be empty!");
+        alert("Password cant be empty!");
         x-=1;
     }
     if( typeof phone==String ||  String(phone).length!=10){
-        window.alert("Phone number is not valid!");
+        alert("Phone number is not valid!");
         x-=1;
     }
     let year=birthdate.slice(0,4);
     year=Number(year);
     if(year>2007){
-        window.alert("Your age is too low");
+        alert("Your age is too low");
         x-=1;
     }
     if(x==0){
@@ -32,6 +32,6 @@ function check(){
 }
 sub.onclick=function(){
     if(check()==0){
-        window.open("home.html");
+        open("home.html");
     }
 }
